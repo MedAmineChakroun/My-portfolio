2a95954df0401c26777fa65be64523fff4c466b9
MedAmineChakroun <164560880+MedAmineChakroun@users.noreply.github.com>
2025-09-02 03:39:11 +0100

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css';
-import Contact from './sections/Contact/Contact';
-import Footer from './sections/Footer/Footer';
-import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
-import Skills from './sections/Skills/Skills';
+import "./App.css";
+import Contact from "./sections/Contact/Contact";
+import Footer from "./sections/Footer/Footer";
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
 
 function App() {
   return (
     <>
       <Hero />
-      <Projects />
       <Skills />
+      <Projects />
       <Contact />
       <Footer />
     </>
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,12 +2,12 @@ import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img-Cqh1d5RO.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
 import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
+import instagramLight from "../../assets/instagraminDark.svg";
+import instagramDark from "../../assets/icons8-instagram-100.png";
 import CV from "../../assets/MyCV.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
@@ -15,9 +15,9 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === "light" ? sun : moon;
-  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
   const githubIcon = theme === "light" ? githubLight : githubDark;
   const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const instagramIcon = theme === "light" ? instagramLight : instagramDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -42,8 +42,8 @@ function Hero() {
         </h1>
         <h2>Full Stack Web Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
+          <a href="https://instagram.com/" target="_blank">
+            <img src={instagramIcon} alt="Instagram icon" />
           </a>
           <a href="https://github.com/" target="_blank">
             <img src={githubIcon} alt="Github icon" />
